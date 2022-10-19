9585558773ba5830767b0edbcfeefc3d46d96673
txw001 <wenjue.txw@raycloud.com>
2022-10-19 14:09:06 +0800

    feat: 添加配置

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
+      <h1>React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
